ce0c1814480a5e5584f3ce97d39767a838538463
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2022-09-24 09:53:41 +0200

    Update api_example.R
    replaced rjson with jsonlite and added writeLines-example

--- api_example.R
+++ api_example.R
@@ -1,7 +1,7 @@
 #----------------
 # Simple Example
 #----------------
-install.packages(c("devtools","rjson","httr"))
+install.packages(c("devtools","jsonlite","httr"))
 devtools::install_github("AndreasFischer1985/qqBaseX")
 clientId="1c852184-1944-4a9e-a093-5cc078981294"
 clientSecret="777f9915-9f0d-4982-9c33-07b5810a3e79"
@@ -14,7 +14,9 @@ token=(httr::content(token_request, as='parsed')$access_token)
 url="https://rest.arbeitsagentur.de/infosysbub/absuche/pc/v1/ausbildungsangebot?uk=Bundesweit&bart=101&sty=0"
 data_request=httr::GET(url=url, httr::add_headers(.headers=c("OAuthAccessToken"=token)),
         config=httr::config(connecttimeout=60))
-data=rjson::fromJSON(rawToChar(httr::content(data_request)))
+data=jsonlite::fromJSON(rawToChar(httr::content(data_request)))
+
+writeLines(jsonlite::toJSON(data$aggregations,pretty=TRUE,auto_unbox=TRUE),paste0(Sys.Date(),"_absuche_bart101_aggregations.json"))
 
 data$aggregations$ANZAHL_GESAMT
 data$aggregations$REGIONEN
@@ -23,13 +25,13 @@ t1=print(Sys.time())
 url="https://rest.arbeitsagentur.de/infosysbub/absuche/pc/v1/ausbildungsangebot?bart=101&sty=0"
 completeData=lapply(sort(gsub("THÜ","TH%C3%9C",names(data$aggregations$REGIONEN)),decreasing=T),function(bl){
         print(paste0("start with ",bl));
-	dataL=rjson::fromJSON(rawToChar(httr::content(httr::GET(url=paste0(url,"&re=",bl), 
+	dataL=jsonlite::fromJSON(rawToChar(httr::content(httr::GET(url=paste0(url,"&re=",bl), 
 			httr::add_headers(.headers=c("OAuthAccessToken"=token)),
         		config=httr::config(connecttimeout=60)))))
 	maxPage=ceiling(dataL$"aggregations"$"ANZAHL_GESAMT"$COUNT/20)
 	lapply(1:maxPage,function(i){
        	 	print(paste0("BL=",bl,"; ",i,"/",maxPage));
-		rjson::fromJSON(rawToChar(httr::content(httr::GET(url=paste0(url,"&pg=",i,"&re=",bl), 
+		jsonlite::fromJSON(rawToChar(httr::content(httr::GET(url=paste0(url,"&pg=",i,"&re=",bl), 
 			httr::add_headers(.headers=c("OAuthAccessToken"=token)),
         		config=httr::config(connecttimeout=60)))))
 	})
